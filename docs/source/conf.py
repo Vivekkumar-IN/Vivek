aae054cbb49be7a0abaccf59a244d31cb3245a47
Vivek Kumar <167422483+Vivekkumar-IN@users.noreply.github.com>
2024-11-17 10:40:10 +0530

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -1,27 +1,19 @@
-# Configuration file for the Sphinx documentation builder.
-#
-# For the full list of built-in configuration values, see the documentation:
-# https://www.sphinx-doc.org/en/master/usage/configuration.html
-
 # -- Project information -----------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
-
 project = 'YukkiMusic'
 copyright = '2024, VivekKumar-IN'
 author = 'VivekKumar-IN'
 
 # -- General configuration ---------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
-
 extensions = []
 
 templates_path = ['_templates']
 exclude_patterns = []
 
-
-
 # -- Options for HTML output -------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
-
 html_theme = 'alabaster'
 html_static_path = ['_static']
+
+# Disable the search box
+html_theme_options = {
+    'search_box': False,  # Disable search box
+}
